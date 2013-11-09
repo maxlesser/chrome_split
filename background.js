dcca9aaec9a1be0218a5bba16a8254fe3ea91a3b
Max <wheels@nyc.rr.com>
2013-11-09 05:26:02 -0500

    cleaned up

--- background.js
+++ background.js
@@ -6,18 +6,11 @@ var x;
 
 for (x = 0; x < allImages.length; x += 1)
 {
-	
-	 //$(allImages[x]).replaceWith( "<div>Kappi xxx is amazing</div>" );
-	 //console.log(allImages[x].src);
-	 var request = "https://lambda-face-detection-and-recognition.p.mashape.com/detect?images=" + allImages[x].src;
-	 console.log(allImages[x]);
+	var request = "https://lambda-face-detection-and-recognition.p.mashape.com/detect?images=" + allImages[x].src;
 
-
-	 // put a mustache image here
-	 $(allImages[x]).replaceWith("<div>" + allImages[x].outerHTML + "<img src='https://h-gvineyards.com/wp-content/uploads/2013/10/moustache_png_by_tatidebieber-d56bqe2.png' id='" + allImages[x].src + "'></img></div>");
-
+	// put a mustache image here
+	$(allImages[x]).replaceWith("<div>" + allImages[x].outerHTML + "<img src='https://h-gvineyards.com/wp-content/uploads/2013/10/moustache_png_by_tatidebieber-d56bqe2.png' id='" + allImages[x].src + "'></img></div>");
 	document.getElementById(allImages[x].src).style.display = 'none';
-
 
 	$.ajax({
 		url: request,
@@ -25,10 +18,6 @@ for (x = 0; x < allImages.length; x += 1)
 		dataType: 'json',
 		success: function(data) 
 		{ 
-
-			// the mustache we need to move based on data
-	
-			console.log(data.photos[0].tags);
 			if (data.photos[0].tags.length != 0){
 				console.log("SUCCESSS BITCHES");
 				document.getElementById(data.images[0]).style.display = 'inline-block';
