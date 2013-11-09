f2a66dd6575898cbdc6b646c20bd67bc0dc36460
Kappibw <kappi.patterson@gmail.com>
2013-11-09 06:39:38 -0500

    musts

--- background.js
+++ background.js
@@ -8,6 +8,7 @@ for (x = 0; x < allImages.length; x += 1)
 	// put a mustache image here
 	$(allImages[x]).replaceWith("<div>" + allImages[x].outerHTML + "<img src='http://i.imgur.com/FoyEVvt.png' id='" + allImages[x].src + "'></img></div>");
 	document.getElementById(allImages[x].src).style.display = 'none';
+	console.log(allImages[x].src);
 
 	$.ajax({
 		url: request,
@@ -19,26 +20,21 @@ for (x = 0; x < allImages.length; x += 1)
 				console.log("SUCCESSS BITCHES");
 				
 				var cur = data.images[0].replace(/\ /g, '%20');
-				console.log(cur);
 				document.getElementById(cur).style.display = 'inline-block';
 				document.getElementById(cur).style.position = 'absolute';
 				document.getElementById(cur).style.marginLeft = -data.photos[0].width;
 				var width = (data.photos[0].tags[0].mouth_right.x - data.photos[0].tags[0].mouth_left.x) * 2;
+				console.log(width);
 				var height = data.photos[0].height*(width/data.photos[0].width);
+				console.log(height);
 				document.getElementById(cur).style.width = width;
 				var XVALUE = data.photos[0].tags[0].mouth_center.x - (width/2);
+				console.log(XVALUE);
 				var YVALUE = data.photos[0].tags[0].mouth_center.y -height/3;
+				console.log(YVALUE + "\n\n");
 				document.getElementById(cur).style.marginLeft = -data.photos[0].width + XVALUE;
 				document.getElementById(cur).style.marginTop = YVALUE;
-
-
-				//$('#' + data.images[0].src);
 			}
-			//if (data.photos[0].tags)
-			// get location of the face from data
-			// move mustache if there is any face data
-			// set hidden to false
-
 		},
 		error: function() {  },
 		beforeSend: setHeader
