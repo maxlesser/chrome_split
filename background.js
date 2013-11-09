d67621423f8dc10d0548cc187b12b2a35f66896b
Kappibw <kappi.patterson@gmail.com>
2013-11-09 05:23:59 -0500

    fixes 4realz!

--- background.js
+++ background.js
@@ -15,9 +15,7 @@ for (x = 0; x < allImages.length; x += 1)
 
 	 // put a mustache image here
 	 $(allImages[x]).replaceWith("<div>" + allImages[x].outerHTML + "<img src='https://h-gvineyards.com/wp-content/uploads/2013/10/moustache_png_by_tatidebieber-d56bqe2.png' id='" + allImages[x].src + "'></img></div>");
-		var test = "#" + allImages[x].src;
-		console.log(test);
-		$(test).hide();
+	document.getElementById(allImages[x].src).style.display = 'none';
 
 	$.ajax({
 		url: request,
@@ -31,7 +29,7 @@ for (x = 0; x < allImages.length; x += 1)
 			console.log(data.photos[0].tags);
 			if (data.photos[0].tags.length != 0){
 				console.log("SUCCESSS BITCHES");
-				console.log($('#' + data.images[0].src));
+				document.getElementById(data.images[0]).style.display = 'inline-block';
 				//$('#' + data.images[0].src);
 			}
 			//if (data.photos[0].tags)
