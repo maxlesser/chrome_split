61b34e4019d31912a5a29fca79b6d9a523c72513
Max <wheels@nyc.rr.com>
2013-11-09 09:29:18 -0500

    stuff

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+
+
 var allImages = $( "img" );
 var x;
 
@@ -6,7 +8,7 @@ for (x = 0; x < allImages.length; x += 1)
 	var request = "https://lambda-face-detection-and-recognition.p.mashape.com/detect?images=" + allImages[x].src;
 
 	// put a mustache image here
-	$(allImages[x]).replaceWith("<div>" + allImages[x].outerHTML + "<img src='http://i.imgur.com/FoyEVvt.png' id='" + allImages[x].src + "' style='display:none'></img></div>");
+	$(allImages[x]).replaceWith("<div><img src='http://i.imgur.com/FoyEVvt.png' id='" + allImages[x].src + "' style='display:none'></img>" + allImages[x].outerHTML + "</div>");
 
 	$.ajax({
 		url: request,
@@ -22,11 +24,13 @@ for (x = 0; x < allImages.length; x += 1)
 				document.getElementById(cur).style.position = 'absolute';
 				document.getElementById(cur).style.marginLeft = -data.photos[0].width + "px";
 				var width = (data.photos[0].tags[0].mouth_right.x - data.photos[0].tags[0].mouth_left.x) * 3;
-				var height = data.photos[0].height*(width/data.photos[0].width);
+
 				document.getElementById(cur).style.width = width + "px";
-				var XVALUE = data.photos[0].tags[0].mouth_center.x - (width/2);
-				var YVALUE = data.photos[0].tags[0].mouth_center.y -height/4;
-				document.getElementById(cur).style.marginLeft = -data.photos[0].width + XVALUE + "px";
+				var XVALUE = data.photos[0].tags[0].mouth_center.x;
+				var mouthAvgX = (data.photos[0].tags[0].mouth_center.x + data.photos[0].tags[0].mouth_left.x + data.photos[0].tags[0].mouth_right.x) / 3;
+				var mouthAvgY = (data.photos[0].tags[0].mouth_center.y + data.photos[0].tags[0].mouth_left.y + data.photos[0].tags[0].mouth_right.y) / 3;
+				var YVALUE = data.photos[0].tags[0].nose.y;
+				document.getElementById(cur).style.marginLeft =  (mouthAvgX - (width/2)) + "px";
 				document.getElementById(cur).style.marginTop = YVALUE + "px";
 			}
 		},
