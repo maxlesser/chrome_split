f000de7c96912898d88fabb878cad14c567dbbbf
Max <wheels@nyc.rr.com>
2013-11-09 13:38:32 -0500

    indentation

--- content.js
+++ content.js
@@ -31,7 +31,7 @@ for (x = 0; x < allImages.length; x += 1)
 		dataType: 'json',
 		success: function(data) 
 		{ 
-				console.log(data);
+			console.log(data);
 			if (data.photos[0] && data.photos[0].tags.length != 0){
 				console.log("successful detection of mustache");
 				var cur = data.images[0].replace(/\ /g, '%20');
@@ -41,20 +41,20 @@ for (x = 0; x < allImages.length; x += 1)
 					document.getElementById(cur).style.position = 'absolute';
 					document.getElementById(cur).style.marginLeft = -data.photos[0].width + "px";
 
-				var ratioX = $('img[src="' + cur + '"]')[0].width / data.photos[0].width;
-				var ratioY = $('img[src="' + cur + '"]')[0].height / data.photos[0].height;
-				console.log($('img[src="' + cur + '"]'));
-				console.log(ratioX);
-				console.log(ratioY);
+					var ratioX = $('img[src="' + cur + '"]')[0].width / data.photos[0].width;
+					var ratioY = $('img[src="' + cur + '"]')[0].height / data.photos[0].height;
+					console.log($('img[src="' + cur + '"]'));
+					console.log(ratioX);
+					console.log(ratioY);
 
-				var width = (data.photos[0].tags[0].mouth_right.x - data.photos[0].tags[0].mouth_left.x) * ratioX * 3;
+					var width = (data.photos[0].tags[0].mouth_right.x - data.photos[0].tags[0].mouth_left.x) * ratioX * 3;
 
-				document.getElementById(cur).style.width = width + "px";
-				var XVALUE = data.photos[0].tags[0].mouth_center.x * ratioX;
-				var mouthAvgX = (data.photos[0].tags[0].mouth_center.x + data.photos[0].tags[0].mouth_left.x + data.photos[0].tags[0].mouth_right.x) / 3 * ratioX;
-				var mouthAvgY = (data.photos[0].tags[0].mouth_center.y + data.photos[0].tags[0].mouth_left.y + data.photos[0].tags[0].mouth_right.y) / 3 * ratioY;
-				var YVALUE = data.photos[0].tags[0].nose.y * ratioY;
-				document.getElementById(cur).style.marginLeft =  (mouthAvgX - (width/2)) + "px";
+					document.getElementById(cur).style.width = width + "px";
+					var XVALUE = data.photos[0].tags[0].mouth_center.x * ratioX;
+					var mouthAvgX = (data.photos[0].tags[0].mouth_center.x + data.photos[0].tags[0].mouth_left.x + data.photos[0].tags[0].mouth_right.x) / 3 * ratioX;
+					var mouthAvgY = (data.photos[0].tags[0].mouth_center.y + data.photos[0].tags[0].mouth_left.y + data.photos[0].tags[0].mouth_right.y) / 3 * ratioY;
+					var YVALUE = data.photos[0].tags[0].nose.y * ratioY;
+					document.getElementById(cur).style.marginLeft =  (mouthAvgX - (width/2)) + "px";
 
 					document.getElementById(cur).style.marginTop = YVALUE + "px";
 				}
