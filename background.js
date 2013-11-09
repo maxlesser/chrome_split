23067cdcd9f6077acbcb045780835e7b257a1700
Max <wheels@nyc.rr.com>
2013-11-09 03:00:13 -0500

    i mustache you a question

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
-chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-    alert('You just typed "' + text + '"');
-  });
+var allImages = $( "img" );
+var x;
+for (x = 0; x < allImages.length; x += 1)
+{
+	$(allImages[x]).replaceWith( "<div>Kappi is amazing</div>" );
+	
+	$.ajax({
+		url: "https://lambda-face-detection-and-recognition.p.mashape.com/detect?images=http%3A%2F%2Fwww.lambdal.com%2Ftest2.jpg",
+		type: 'GET',
+		dataType: 'json',
+		success: function(data) 
+		{ 
+			alert('hello!'); 
+		},
+		error: function() { alert('sadness!'); },
+		beforeSend: setHeader
+	});
 
-chrome.omnibox.setDefaultSuggestion(
-	{description: 'DESCRIPTION?!?'});
+	function setHeader(xhr) {
+		xhr.setRequestHeader('X-Mashape-Authorization', 'E4SntsExPG3lTSYKunjBQmVMJIbMtHFc');
+	}
+}
