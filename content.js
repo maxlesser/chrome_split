f2c45b583978a20d6cdefacf8e1bd700dfe1f7d6
Luke Fiorante <luke.fiorante@gmail.com>
2013-11-09 13:28:38 -0500

    No more nulls

--- content.js
+++ content.js
@@ -31,24 +31,26 @@ for (x = 0; x < allImages.length; x += 1)
 		success: function(data) 
 		{ 
 				console.log(data);
-			if (data.photos[0].tags.length != 0){
+			if (data.photos[0] && data.photos[0].tags.length != 0){
 				console.log("successful detection of mustache");
 				var cur = data.images[0].replace(/\ /g, '%20');
-				document.getElementById(cur).style.display = 'inline-block';
-				document.getElementById(cur).style.position = 'absolute';
-				document.getElementById(cur).style.marginLeft = -data.photos[0].width + "px";
+				if (document.getElementById(cur)) {
+					document.getElementById(cur).style.display = 'inline-block';
+					document.getElementById(cur).style.position = 'absolute';
+					document.getElementById(cur).style.marginLeft = -data.photos[0].width + "px";
 
 
-				var width = (data.photos[0].tags[0].mouth_right.x - data.photos[0].tags[0].mouth_left.x) * 3;
+					var width = (data.photos[0].tags[0].mouth_right.x - data.photos[0].tags[0].mouth_left.x) * 3;
 
-				document.getElementById(cur).style.width = width + "px";
-				var XVALUE = data.photos[0].tags[0].mouth_center.x;
-				var mouthAvgX = (data.photos[0].tags[0].mouth_center.x + data.photos[0].tags[0].mouth_left.x + data.photos[0].tags[0].mouth_right.x) / 3;
-				var mouthAvgY = (data.photos[0].tags[0].mouth_center.y + data.photos[0].tags[0].mouth_left.y + data.photos[0].tags[0].mouth_right.y) / 3;
-				var YVALUE = data.photos[0].tags[0].nose.y;
-				document.getElementById(cur).style.marginLeft =  (mouthAvgX - (width/2)) + "px";
+					document.getElementById(cur).style.width = width + "px";
+					var XVALUE = data.photos[0].tags[0].mouth_center.x;
+					var mouthAvgX = (data.photos[0].tags[0].mouth_center.x + data.photos[0].tags[0].mouth_left.x + data.photos[0].tags[0].mouth_right.x) / 3;
+					var mouthAvgY = (data.photos[0].tags[0].mouth_center.y + data.photos[0].tags[0].mouth_left.y + data.photos[0].tags[0].mouth_right.y) / 3;
+					var YVALUE = data.photos[0].tags[0].nose.y;
+					document.getElementById(cur).style.marginLeft =  (mouthAvgX - (width/2)) + "px";
 
-				document.getElementById(cur).style.marginTop = YVALUE + "px";
+					document.getElementById(cur).style.marginTop = YVALUE + "px";
+				}
 			}
 		},
 		error: function() {  },
